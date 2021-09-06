d3a4da00e5dff73aa1b9a781b37ce5427b391505
jessan <jessenia.garcia998@gmail.com>
2021-09-06 15:04:26 -0600

    server.js brings up test notes

---
+++ routes/index.js
@@ -0,0 +1,14 @@
+// const fs = require('fs')
+// const path = require('path');
+// const router = require('express').Router();
+// const dataNotes = require('../db/db.json');
+
+// router.get('/api/notes', (req, res) => {
+//     res.json(dataNotes);
+// })
+
+// router.get('/api/notes/:id', (req, res) => {
+    
+// })
+
+// module.exports = router;
--- server.js
+++ server.js
@@ -1,6 +1,15 @@
+const fs = require('fs')
+const path = require('path')
 const express = require('express');
 const PORT = process.env.PORT || 3001
 const app = express();
+const { notes } = require('./db/db.json')
+//const routes = require('./routes/index')
+
+app.get('/api/notes', (req, res) => {
+    res.json(notes);
+});
+
 
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}`);
